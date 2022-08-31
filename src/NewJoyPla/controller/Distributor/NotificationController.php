79a64c5c75df972b94f288c3c23ea694b86b5801
ito.shun <ito.shun@pi-pe.co.jp>
2022-08-31 00:20:12 +0000

    Squashed commit of the following
    commit 9db6a82d 
    Author: iida <iida.saaya@pi-pe.co.jp> 
    Date: Tue Aug 30 2022 19:31:34 GMT+0900 (日本標準時) 
    
        単体テスト完了

--- src/NewJoyPla/controller/Distributor/NotificationController.php
+++ src/NewJoyPla/controller/Distributor/NotificationController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use View;
@@ -10,6 +11,7 @@ use App\Lib\UserInfo;
 use App\Model\Stock;
 use App\Model\Lot;
 use App\Model\OrderHistory;
+use App\Model\DistributorAffiliationView;
 
 use ApiErrorCode\FactoryApiErrorCode;
 use stdClass;
@@ -17,73 +19,77 @@ use Exception;
 
 class NotificationController extends Controller
 {
-    
     public $links = [
         'unorderedList' => '%url/rel:mpgt:Order%&Action=unorderedList',
         'stock' => '%url/rel:mpgt:Stock%&Action=stockManagementList',
-        'lot' => '%url/rel:mpgt:Lots%&Action=lotManagementList'
+        'lot' => '%url/rel:mpgt:Lots%&Action=lotManagementList',
+        'undoneOrder' => '%url/rel:mpgt:OrderD%'
         ];
-    
+
     public $icons = [
         'unorderedList' => "https://i02.smp.ne.jp/u/joypla/images/menu_icon/1.png",
         'stock' => "https://i02.smp.ne.jp/u/joypla/images/menu_icon/6.png",
         'lot' => "https://i02.smp.ne.jp/u/joypla/images/menu_icon/6.png",
+        'undoneOrder' => "https://i02.smp.ne.jp/u/joypla/images/menu_icon/6.png"
     ];
-    
+
     public function __construct()
     {
     }
-    
+
     public function index(): View
     {
         global $SPIRAL;
         try {
             $user_info = new UserInfo($SPIRAL);
             $notification = [];
-            if($user_info->isHospitalUser())
-            {
-                $instance =  Stock::where('hospitalId',$user_info->getHospitalId())->where('stockQuantity', 0 , '<');
-                if($user_info->isUser()){$instance = $instance->where('divisionId',$user_info->getDivisionId());}
+            if ($user_info->isHospitalUser()) {
+                $instance =  Stock::where('hospitalId', $user_info->getHospitalId())->where('stockQuantity', 0, '<');
+                if ($user_info->isUser()) {
+                    $instance = $instance->where('divisionId', $user_info->getDivisionId());
+                }
                 $count = $instance->count();
-                if($count > 0)
-                {
+                if ($count > 0) {
                     $notification[] = [
                         'message' => '在庫管理表にマイナスとなっている在庫が<br><span class="uk-text-danger uk-text-bold">'.$count.'件</span>存在します',
                         'icon' => $this->icons['stock'],
                         'link' => $this->links['stock'],
                     ];
                 }
-                
-                $instance =  Lot::where('hospitalId',$user_info->getHospitalId())->where('stockQuantity', 0 , '<');
-                if($user_info->isUser()){$instance = $instance->where('divisionId',$user_info->getDivisionId());}
+
+                $instance =  Lot::where('hospitalId', $user_info->getHospitalId())->where('stockQuantity', 0, '<');
+                if ($user_info->isUser()) {
+                    $instance = $instance->where('divisionId', $user_info->getDivisionId());
+                }
                 $count = $instance->count();
-                if($count > 0)
-                {
+                if ($count > 0) {
                     $notification[] = [
                         'message' => 'ロット管理表にマイナスとなっている在庫が<br><span class="uk-text-danger uk-text-bold">'.$count.'件</span>存在します',
                         'icon' => $this->icons['lot'],
                         'link' => $this->links['lot'],
                     ];
                 }
-                
-                $month_3 = date("Y/m/d",strtotime("+3 month"));
-                $instance =  Lot::where('hospitalId',$user_info->getHospitalId())->where('lotDate', $month_3 , '<');
-                if($user_info->isUser()){$instance = $instance->where('divisionId',$user_info->getDivisionId());}
+
+                $month_3 = date("Y/m/d", strtotime("+3 month"));
+                $instance =  Lot::where('hospitalId', $user_info->getHospitalId())->where('lotDate', $month_3, '<');
+                if ($user_info->isUser()) {
+                    $instance = $instance->where('divisionId', $user_info->getDivisionId());
+                }
                 $count = $instance->count();
-                if($count > 0)
-                {
+                if ($count > 0) {
                     $notification[] = [
                         'message' => 'ロット管理表に期限が3カ月以内に切れる商品が<br><span class="uk-text-danger uk-text-bold">'.$count.'件</span>存在します',
                         'icon' => $this->icons['lot'],
                         'link' => $this->links['lot'],
                     ];
                 }
-                
-                $instance =  OrderHistory::where('hospitalId',$user_info->getHospitalId())->where('orderStatus', '1');
-                if($user_info->isUser()){$instance = $instance->where('divisionId',$user_info->getDivisionId());}
+
+                $instance =  OrderHistory::where('hospitalId', $user_info->getHospitalId())->where('orderStatus', '1');
+                if ($user_info->isUser()) {
+                    $instance = $instance->where('divisionId', $user_info->getDivisionId());
+                }
                 $count = $instance->count();
-                if($count > 0)
-                {
+                if ($count > 0) {
                     $notification[] = [
                         'message' => '未発注伝票が<br><span class="uk-text-danger uk-text-bold">'.$count.'件</span>存在します',
                         'icon' => $this->icons['unorderedList'],
@@ -91,17 +97,36 @@ class NotificationController extends Controller
                     ];
                 }
             }
-            
-            $content = new ApiResponse($notification , count($notification) , 0 , '', ['']);
+
+            if ($user_info->isDistributorUser()) {
+                $distributorAffiliations = DistributorAffiliationView::where('loginId', $user_info->getLoginId())->where('invitingAgree', '1')->get();
+                $distributorAffiliations = $distributorAffiliations->data->all();
+
+                $instance = OrderHistory::where('orderStatus', '1', '>')->where('orderStatus', '6', '<');
+
+                foreach ($distributorAffiliations as $affiliation) {
+                    $instance->orWhere('distributorId', $affiliation->distributorId);
+                }
+
+                $count = $instance->count();
+                if ($count > 0) {
+                    $notification[] = [
+                        'message' => '納品未完了伝票が<br><span class="uk-text-danger uk-text-bold">'.$count.'件</span>存在します',
+                        'icon' => $this->icons['undoneOrder'],
+                        'link' => $this->links['undoneOrder'],
+                    ];
+                }
+            }
+
+            $content = new ApiResponse($notification, count($notification), 0, '', ['']);
             $content = $content->toJson();
-            
-        } catch ( Exception $ex ) {
-            $content = new ApiResponse([], 0 , $ex->getCode(), $ex->getMessage(), ['']);
+        } catch (Exception $ex) {
+            $content = new ApiResponse([], 0, $ex->getCode(), $ex->getMessage(), ['']);
             $content = $content->toJson();
-        }finally {
+        } finally {
             return $this->view('NewJoyPla/view/template/ApiResponse', [
                 'content'   => $content,
-            ],false);
+            ], false);
         }
     }
 }
