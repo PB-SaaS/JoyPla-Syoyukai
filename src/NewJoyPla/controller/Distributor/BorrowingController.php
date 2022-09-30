6b57698d14bb52a9ec6ae1d064b68c9bde5b125f
iida <iida.saaya@pi-pe.co.jp>
2022-09-30 12:55:47 +0900

    処理変更、不要関数削除

--- src/NewJoyPla/controller/Distributor/BorrowingController.php
+++ src/NewJoyPla/controller/Distributor/BorrowingController.php
@@ -22,7 +22,6 @@ use App\Model\Hospital;
 use App\Model\HospitalUser;
 use App\Model\Distributor;
 use App\Model\DistributorUser;
-use App\Model\AssociationTR;
 use App\Model\InHospitalItemView;
 
 use ApiErrorCode\FactoryApiErrorCode;
@@ -67,15 +66,7 @@ class BorrowingController extends Controller
                 'SPIRAL' => $SPIRAL
             ], false);
 
-            if ($user_info->isAdmin() || $user_info->isApprover()) {
-                $borrowingAction = 'borrowingRegistrationToUsedReportApi';
-            }
-            if ($user_info->isUser()) {
-                $borrowingAction = 'borrowingRegistrationToUnapprovedUsedSlipApi';
-            }
-            if ($user_info->isDistributorUser()) {
-                $borrowingAction = '';
-            }
+            $borrowingAction = '';
 
             $hospital = Hospital::where('hospitalId', $user_info->getHospitalId())->get();
             $hospital = $hospital->data->get(0);
@@ -269,59 +260,6 @@ class BorrowingController extends Controller
         }
     }
 
-    public function borrowingRegistrationToUnapprovedUsedSlipApi()
-    {
-        global $SPIRAL;
-        try {
-            $token = (!isset($_POST['_csrf'])) ? '' : $_POST['_csrf'];
-            Csrf::validate($token, true);
-
-            $user_info = new UserInfo($SPIRAL);
-
-            if ($user_info->isDistributorUser()) {
-                throw new Exception(FactoryApiErrorCode::factory(191)->getMessage(), FactoryApiErrorCode::factory(191)->getCode());
-            }
-
-            if (! ($user_info->isHospitalUser() && $user_info->isUser())) {
-                throw new Exception(FactoryApiErrorCode::factory(191)->getMessage(), FactoryApiErrorCode::factory(191)->getCode());
-            }
-            $borrowing_regist_result = $this->borrowingRegist();
-
-            $used_slip_create_data = $this->usedSlipHisotoyRegist($borrowing_regist_result->insert_data, 1);
-
-            $all_create_data = $this->usedReportApi($borrowing_regist_result->insert_data);
-
-            $result = $this->association($used_slip_create_data['ids'], $all_create_data['ids']);
-
-            $content = new ApiResponse($result->data, $result->count, $result->code, $result->message, ['insert']);
-            $content = $content->toJson();
-        } catch (Exception $ex) {
-            $content = new ApiResponse([], 0, $ex->getCode(), $ex->getMessage(), ['insert']);
-            $content = $content->toJson();
-        } finally {
-            return $this->view('NewJoyPla/view/template/ApiResponse', [
-                'content'   => $content,
-            ], false);
-        }
-    }
-
-    private function association(array $used_slip_ids, array $other_ids)
-    {
-        $insert_data = [];
-        foreach ($used_slip_ids as $divisionId_distributorId_usedDate => $data) {
-            $insert_data[] = [
-                'usedSlipId' => $used_slip_ids[$divisionId_distributorId_usedDate]['usedSlipId'],
-                'orderNumber' => $other_ids[$divisionId_distributorId_usedDate]['orderHistoryId'],
-                'receivingHId' => $other_ids[$divisionId_distributorId_usedDate]['receivingHistoryId'],
-                'billingNumber' => $other_ids[$divisionId_distributorId_usedDate]['billingHistoryId'],
-            ];
-        }
-
-        $result = AssociationTR::insert($insert_data);
-
-        return $result;
-    }
-
     private function usedSlipHisotoyRegist(array $borrowing_insert_items, int $used_slip_status = 1)
     {
         global $SPIRAL;
@@ -454,6 +392,10 @@ class BorrowingController extends Controller
                     break;
                 }
             }
+
+            if (!$exist) {
+                throw new Exception('Borrowing item does not belong to the distributor', 999);
+            }
             if ($lot_flag && $item['lotNumber'] == '' && $item['lotDate'] == '') {
                 throw new Exception('invalid lot', 100);
             }
@@ -466,15 +408,14 @@ class BorrowingController extends Controller
                     throw new Exception('invalid lotNumber format', 102);
                 }
             }
-            if ($exist) {
-                $insert_data[$key] = [
-                    'inHospitalItemId' => $item['recordId'],
-                    'borrowingNum' => 1,
-                    'lotNumber' => $item['lotNumber'],
-                    'lotDate' => $item['lotDate'],
-                    'divisionId' => $divisionId,
-                ];
-            }
+
+            $insert_data[$key] = [
+                'inHospitalItemId' => $item['recordId'],
+                'borrowingNum' => 1,
+                'lotNumber' => $item['lotNumber'],
+                'lotDate' => $item['lotDate'],
+                'divisionId' => $divisionId,
+            ];
         }
         $result = Borrowing::insert($insert_data);
 
@@ -493,252 +434,6 @@ class BorrowingController extends Controller
         }
         return $result;
     }
-
-    private function usedReportApi(array $borrowing_data)
-    {
-        global $SPIRAL;
-        $all_create_data = [ 'ids' => [] , 'borrowingData' => []];
-
-        $user_info = new UserInfo($SPIRAL);
-
-        $hospital = Hospital::where('hospitalId', $user_info->getHospitalId())->get();
-        $hospital = $hospital->data->all();
-        if ($this->in_hospital_items == null) {
-            $in_hospital_items_instance = InHospitalItem::getInstance();
-            foreach ($borrowing_data as $data) {
-                $in_hospital_items_instance = $in_hospital_items_instance::orWhere('inHospitalItemId', $data->inHospitalItemId);
-            }
-            $this->in_hospital_items = $in_hospital_items_instance::get();
-        }
-        $used_report = [];
-        foreach ($borrowing_data as $borrowing_key => $data) {
-            $used_report[$borrowing_key] = new StdClass();
-            foreach ($this->in_hospital_items->data as $key => $val) {
-                if ($val->inHospitalItemId == $data->inHospitalItemId) {
-                    $used_report[$borrowing_key]->inHospitalItemId = $val->inHospitalItemId;
-                    $used_report[$borrowing_key]->hospitalId = $val->hospitalId;
-                    $used_report[$borrowing_key]->distributorId = $val->distributorId;
-                    $used_report[$borrowing_key]->catalogNo = $val->catalogNo;
-                    $used_report[$borrowing_key]->serialNo = $val->serialNo;
-                    $used_report[$borrowing_key]->quantity = $val->quantity;
-                    $used_report[$borrowing_key]->quantityUnit = $val->quantityUnit;
-                    $used_report[$borrowing_key]->itemUnit = $val->itemUnit;
-                    $used_report[$borrowing_key]->notUsedFlag = $val->notUsedFlag;
-                    $used_report[$borrowing_key]->itemId = $val->itemId;
-                    $used_report[$borrowing_key]->itemName = $val->itemName;
-                    $used_report[$borrowing_key]->itemCode = $val->itemCode;
-                    $used_report[$borrowing_key]->itemStandard = $val->itemStandard;
-                    $used_report[$borrowing_key]->itemJANCode = $val->itemJANCode;
-                    $used_report[$borrowing_key]->notice = $val->notice;
-                    $used_report[$borrowing_key]->makerName = $val->makerName;
-                    $used_report[$borrowing_key]->price = $val->price;
-                    $used_report[$borrowing_key]->unitPrice = $val->unitPrice;
-
-                    $used_report[$borrowing_key]->countNum = $data->borrowingNum;
-                    $used_report[$borrowing_key]->lotNumber = $data->lotNumber;
-                    $used_report[$borrowing_key]->lotDate = $data->lotDate;
-                    $used_report[$borrowing_key]->divisionId = $data->divisionId;
-                    $used_report[$borrowing_key]->usedDate = $data->usedDate;
-                }
-            }
-        }
-        $all_create_data['borrowingData'] = $used_report;
-
-        $history_ids = [];
-        foreach ($used_report as $item) {
-            if (! isset($history_ids[$item->divisionId . $item->distributorId . $item->usedDate]) || ! $history_ids[$item->divisionId . $item->distributorId . $item->usedDate]) {
-                $order_id = $this->makeId('03');
-                $receiving_id = $this->makeId('04');
-                $billing_id = $this->makeId('02');
-                $history_ids[$item->divisionId . $item->distributorId . $item->usedDate] = [
-                    'orderHistoryId' => $order_id,
-                    'receivingHistoryId' => $receiving_id,
-                    'billingHistoryId' => $billing_id,
-                    'divisionId' => $item->divisionId,
-                    'distributorId' => $item->distributorId,
-                    'usedDate' => $item->usedDate,
-                ];
-            }
-        }
-        $all_create_data['ids'] = $history_ids;
-        /****** 発注登録 ******/
-
-        $order_insert_data = [];
-        foreach ($used_report as $item) {
-            if (!$order_insert_data[$item->inHospitalItemId]) {
-                $order_insert_data[$item->inHospitalItemId] = [
-                    'registrationTime' => $item->usedDate,
-                    'orderTime' => $item->usedDate,
-                    'receivingTime' => $item->usedDate,
-                    'orderCNumber' => $this->makeId('BO'),
-                    'hospitalId' => $item->hospitalId,
-                    'inHospitalItemId' => $item->inHospitalItemId,
-                    'orderNumber' => $history_ids[$item->divisionId . $item->distributorId . $item->usedDate]['orderHistoryId'],
-                    'price' => $item->price,
-                    'orderQuantity' => 0,
-                    'orderPrice' => 0,
-                    'receivingFlag' => 1,
-                    'receivingNum' => 0,
-                    'quantity' => $item->quantity,
-                    'quantityUnit' => $item->quantityUnit,
-                    'itemUnit' => $item->itemUnit,
-                    'divisionId' => $item->divisionId,
-                    'distributorId' => $item->distributorId,
-                ];
-            }
-            $order_insert_data[$item->inHospitalItemId]['orderQuantity'] = ($order_insert_data[$item->inHospitalItemId]['orderQuantity'] + (float)$item->countNum);
-            $order_insert_data[$item->inHospitalItemId]['orderPrice'] = $order_insert_data[$item->inHospitalItemId]['orderPrice'] + ((float)$item->price * (float)$item->countNum);
-            $order_insert_data[$item->inHospitalItemId]['receivingNum'] = $order_insert_data[$item->inHospitalItemId]['receivingNum'] + (float)$item->countNum;
-        }
-
-        $order_history_insert_data = [];
-        foreach ($history_ids as $divisionId_distributorId_usedDate => $history_data) {
-            $order_price = [];
-            $in_hospital_item_ids = [];
-            foreach ($order_insert_data as $insert_record) {
-                if ($insert_record['orderNumber'] === $history_data['orderHistoryId']) {
-                    $order_price[] = (float)$insert_record['orderPrice'];
-                    if (! in_array($insert_record['inHospitalItemId'], $in_hospital_item_ids)) {
-                        $in_hospital_item_ids[] = $insert_record['inHospitalItemId'];
-                    }
-                }
-            }
-            $order_history_insert_data[] = [
-                'registrationTime' => $history_data['usedDate'],
-                'orderTime' => $history_data['usedDate'],
-                'receivingTime' => $history_data['usedDate'],
-                'orderNumber' => $history_data['orderHistoryId'],
-                'hospitalId' => $user_info->getHospitalId(),
-                'divisionId' => $history_data['divisionId'],
-                'itemsNumber' => collect($in_hospital_item_ids)->count(),
-                'totalAmount' => collect($order_price)->sum(),
-                'orderStatus' => 8,
-                'distributorId' => $history_data['distributorId'],
-                'ordererUserName' => $user_info->getName(),
-                'ordercomment' => '貸出品',
-            ];
-        }
-        /****** 発注登録 ******/
-        /****** 入荷登録 ******/
-
-        $receiving_insert_data = [];
-        foreach ($used_report as $item) {
-            foreach ($order_insert_data as $order_data) {
-                $orderCNumber = '';
-                if ($item->inHospitalItemId == $order_data['inHospitalItemId']) {
-                    $orderCNumber = $order_data['orderCNumber'];
-                    break; //ループ終了
-                }
-            }
-            $receiving_insert_data[] = [
-                'registrationTime' => $item->usedDate,
-                'orderCNumber' => $orderCNumber,
-                'receivingCount' => (float)$item->countNum,
-                'receivingHId' => $history_ids[$item->divisionId . $item->distributorId . $item->usedDate]['receivingHistoryId'],
-                'inHospitalItemId' => $item->inHospitalItemId,
-                'price' => (float)$item->price,
-                'receivingPrice' => (float)$item->price * (float)$item->countNum,
-                'hospitalId' => $item->hospitalId,
-                'totalReturnCount' => 0,
-                'divisionId' => $item->divisionId,
-                'distributorId' => $item->distributorId,
-                'adjAmount' => 0,
-                'priceAfterAdj' => 0,
-                'lotNumber' => $item->lotNumber,
-                'lotDate' => $item->lotDate,
-            ];
-        }
-
-        $receiving_history_insert_data = [];
-        foreach ($history_ids as $divisionId_distributorId_usedDate => $history_data) {
-            //$receiving_price = [];
-            $in_hospital_item_ids = [];
-            foreach ($receiving_insert_data as $insert_record) {
-                if ($insert_record['receivingHId'] === $history_data['receivingHistoryId']) {
-                    //$receiving_price[] = (float)$insert_record['receivingPrice'];
-                    if (! in_array($insert_record['inHospitalItemId'], $in_hospital_item_ids)) {
-                        $in_hospital_item_ids[] = $insert_record['inHospitalItemId'];
-                    }
-                }
-            }
-            $receiving_history_insert_data[] = [
-                'registrationTime' => $history_data['usedDate'],
-                'receivingHId' => $history_data['receivingHistoryId'],
-                'distributorId' => $history_data['distributorId'],
-                'orderHistoryId' => $history_data['orderHistoryId'],
-                'hospitalId' => $user_info->getHospitalId(),
-                'itemsNumber' => collect($in_hospital_item_ids)->count(),
-                'divisionId' => $history_data['divisionId'],
-                'recevingStatus' => 2,
-            ];
-        }
-
-        /****** 入荷登録 ******/
-        /****** 消費登録 ******/
-        $billing_insert_data = [];
-        foreach ($used_report as $item) {
-            $unitPrice = ($hospital->billingUnitPrice)
-                ? (float)$item->unitPrice
-                : (((float)$item->price == 0 || (float)$item->quantity == 0)
-                    ? 0
-                    : (float)$item->price / (float)$item->quantity);
-            $billing_insert_data[] = [
-                'registrationTime' => $item->usedDate,
-                'inHospitalItemId' => $item->inHospitalItemId,
-                'billingNumber' => $history_ids[$item->divisionId . $item->distributorId . $item->usedDate]['billingHistoryId'],
-                'price' => (float)$item->price,
-                'billingQuantity' => ((float)$item->quantity * (float)$item->countNum),
-                'billingAmount' => $unitPrice * ((float)$item->quantity * (float)$item->countNum),
-                'hospitalId' => $item->hospitalId,
-                'divisionId' => $item->divisionId,
-                'quantity' => $item->quantity,
-                'quantityUnit' => $item->quantityUnit,
-                'itemUnit' => $item->itemUnit,
-                'lotNumber' => $item->lotNumber,
-                'lotDate' => $item->lotDate,
-                'unitPrice' => $item->unitPrice,
-            ];
-        }
-
-        $billing_history_insert_data = [];
-        foreach ($history_ids as $divisionId_distributorId_usedDate => $history_data) {
-            $total_amount = [];
-            $in_hospital_item_ids = [];
-            foreach ($billing_insert_data as $insert_record) {
-                if ($insert_record['billingNumber'] === $history_data['billingHistoryId']) {
-                    $total_amount[] = (float)$insert_record['billingAmount'];
-                    if (! in_array($insert_record['inHospitalItemId'], $in_hospital_item_ids)) {
-                        $in_hospital_item_ids[] = $insert_record['inHospitalItemId'];
-                    }
-                }
-            }
-            $billing_history_insert_data[] = [
-                'registrationTime' => $history_data['usedDate'],
-                'billingDate' => $history_data['usedDate'],
-                'billingNumber' => $history_data['billingHistoryId'],
-                'hospitalId' => $user_info->getHospitalId(),
-                'divisionId' => $history_data['divisionId'],
-                'itemsNumber' => collect($in_hospital_item_ids)->count(),
-                'totalAmount' => collect($total_amount)->sum()
-            ];
-        }
-
-        /****** 貸出更新 ******/
-        $result = OrderHistory::insert($order_history_insert_data);
-
-        $result = Order::insert($order_insert_data);
-
-        $result = ReceivingHistory::insert($receiving_history_insert_data);
-
-        $result = Receiving::insert($receiving_insert_data);
-
-        $result = BillingHistory::insert($billing_history_insert_data);
-
-        $result = Billing::insert($billing_insert_data);
-
-        return $all_create_data;
-    }
-
 
     public function usedTemporaryReportApi()
     {
