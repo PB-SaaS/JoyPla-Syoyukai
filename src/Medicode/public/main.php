7a47bef68fdc0b02114a5f111aa18e6c151cc02a
ito.shun <ito.shun@pi-pe.co.jp>
2022-12-23 12:18:20 +0900

    名前空間の設定

--- src/Medicode/public/main.php
+++ src/Medicode/public/main.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-require_once "Medicode/autoload.php";
-require_once "Medicode/public/function.php";
+require_once 'Medicode/autoload.php';
+require_once 'Medicode/public/function.php';
 
-use Exception;
 use Medicode\Shared\Exceptions\IAppException;
 use Medicode\Shared\Exceptions\ApiException;
 use Medicode\Shared\Exceptions\ValidationException;
@@ -22,32 +21,40 @@ $errors = [];
 $logs = [];
 
 try {
-    
     $action = new SendOrderBatchController(
-        new GetAuthenticationInteractor(new MedicodeAuthenticationRepository(), new SPIRALAuthenticationRepository()),
-        new UpdateAccessTokenInteractor(new MedicodeAuthenticationRepository(), new SPIRALAuthenticationRepository()),
-        new SendOrderInteractor(new OrderRepository()));
+        new GetAuthenticationInteractor(
+            new MedicodeAuthenticationRepository(),
+            new SPIRALAuthenticationRepository()
+        ),
+        new UpdateAccessTokenInteractor(
+            new MedicodeAuthenticationRepository(),
+            new SPIRALAuthenticationRepository()
+        ),
+        new SendOrderInteractor(new OrderRepository())
+    );
     $result = $action->index();
-    
 } catch (IAppException $ex) {
-    
     $errors[] = [
         'now',
-        date("Y-m-d H:i:s") . "." . substr(explode(".", (microtime(true) . ".000"))[1], 0, 3),
+        date('Y-m-d H:i:s') .
+        '.' .
+        substr(explode('.', microtime(true) . '.000')[1], 0, 3),
         $ex->getCode(),
-        $ex->getMessage()."\n".'[ '.$ex->getFile().' line: '.$ex->getLine().' ]'
+        $ex->getMessage() .
+        "\n" .
+        '[ ' .
+        $ex->getFile() .
+        ' line: ' .
+        $ex->getLine() .
+        ' ]',
     ];
-    
 } catch (Exception $e) {
-    
     echo $e;
-    
 } finally {
-    
     $ok = 0;
     $ng = 0;
-    $total = ($result['data']) ? $result['data']->count() : 0;
-    
+    $total = $result['data'] ? $result['data']->count() : 0;
+
     if ($result['data']) {
         foreach ($result['data'] as $order) {
             if ($order->getIsValid()) {
@@ -58,38 +65,44 @@ try {
             }
         }
     }
-    
-    if ($ok > 0 || $result['code'] === 0)
-    {
+
+    if ($ok > 0 || $result['code'] === 0) {
         $logs[] = [
             'now',
             '',
             $result['code'],
-            $result['message']."\n Total = ".$total." Success = ".$ok." Error = ".$ng
+            $result['message'] .
+            "\n Total = " .
+            $total .
+            ' Success = ' .
+            $ok .
+            ' Error = ' .
+            $ng,
         ];
     }
-    
-    if ($ng > 0)
-    {
+
+    if ($ng > 0) {
         $errors[] = [
             'now',
-            date("Y-m-d H:i:s") . "." . substr(explode(".", (microtime(true) . ".000"))[1], 0, 3),
+            date('Y-m-d H:i:s') .
+            '.' .
+            substr(explode('.', microtime(true) . '.000')[1], 0, 3),
             600,
-            "対象の発注データにフォーマットエラーのものがあります。"."\n"."■フォーマットエラー件数: {$ng}/{$total}"
+            '対象の発注データにフォーマットエラーのものがあります。' .
+            "\n" .
+            "■フォーマットエラー件数: {$ng}/{$total}",
         ];
     }
-    
-    if (count($errors) > 0)
-    {
+
+    if (count($errors) > 0) {
         $logs = array_merge($logs, $errors);
         notifyError($errors);
     }
-    
+
     bulkInsertLog($logs);
-    
-    echo "results..."."\n";
-    foreach ($logs as $log)
-    {
-        echo "code: ".$log[2]."\n".$log[3]."\n\n";
+
+    echo 'results...' . "\n";
+    foreach ($logs as $log) {
+        echo 'code: ' . $log[2] . "\n" . $log[3] . "\n\n";
     }
 }
--- src/NewJoyPla/autoload.php
+++ src/NewJoyPla/autoload.php
@@ -1,46 +1,47 @@
 <?php
 
 use App\Lib\ApiSpiral;
+use NewJoyPla\lib\Spiralv2LogginObject;
 
-require_once "NewJoyPla/core/Monad.php";
-require_once "NewJoyPla/core/Collection.php";
-require_once "NewJoyPla/core/Model.php";
-require_once "NewJoyPla/core/View.php";
-require_once "NewJoyPla/core/Controller.php";
-require_once "NewJoyPla/core/ApiController.php";
-require_once "NewJoyPla/core/Response.php";
-require_once "NewJoyPla/core/ApiResponse.php";
-require_once "NewJoyPla/core/ListResponse.php";
-require_once "NewJoyPla/core/Form.php";
-require_once "NewJoyPla/core/Field.php";
-require_once "NewJoyPla/core/FieldSet.php";
-require_once "NewJoyPla/core/Util.php";
-require_once "NewJoyPla/core/Csrf.php";
+require_once 'NewJoyPla/core/Monad.php';
+require_once 'NewJoyPla/core/Collection.php';
+require_once 'NewJoyPla/core/Model.php';
+require_once 'NewJoyPla/core/View.php';
+require_once 'NewJoyPla/core/Controller.php';
+require_once 'NewJoyPla/core/ApiController.php';
+require_once 'NewJoyPla/core/Response.php';
+require_once 'NewJoyPla/core/ApiResponse.php';
+require_once 'NewJoyPla/core/ListResponse.php';
+require_once 'NewJoyPla/core/Form.php';
+require_once 'NewJoyPla/core/Field.php';
+require_once 'NewJoyPla/core/FieldSet.php';
+require_once 'NewJoyPla/core/Util.php';
+require_once 'NewJoyPla/core/Csrf.php';
 
-
-require_once "NewJoyPla/lib/ApiSpiral.php";
-require_once "NewJoyPla/lib/Logger.php";
-require_once "NewJoyPla/lib/LoggingSpiralv2.php";
+require_once 'NewJoyPla/lib/ApiSpiral.php';
+require_once 'NewJoyPla/lib/Logger.php';
+require_once 'NewJoyPla/lib/LoggingSpiralv2.php';
 require_once 'NewJoyPla/lib/UserInfo.php';
-require_once "NewJoyPla/lib/SpiralDataBase.php";
-require_once "NewJoyPla/lib/SpiralSendMail.php";
-require_once "NewJoyPla/lib/SpiralTable.php";
-require_once "NewJoyPla/lib/SpiralArea.php";
-require_once "NewJoyPla/lib/SpiralDBFilter.php";
-require_once "NewJoyPla/lib/Define.php";
-require_once "NewJoyPla/lib/Func.php";
+require_once 'NewJoyPla/lib/SpiralDataBase.php';
+require_once 'NewJoyPla/lib/SpiralSendMail.php';
+require_once 'NewJoyPla/lib/SpiralTable.php';
+require_once 'NewJoyPla/lib/SpiralArea.php';
+require_once 'NewJoyPla/lib/SpiralDBFilter.php';
+require_once 'NewJoyPla/lib/Define.php';
+require_once 'NewJoyPla/lib/Func.php';
 
-require_once "NewJoyPla/model/Base.php";
+require_once 'NewJoyPla/model/Base.php';
 
-require_once "NewJoyPla/core/HttpRequest.php";
-require_once "NewJoyPla/core/HttpRequestParameter.php";
-require_once "NewJoyPla/lib/Spiralv2LogginObject.php";
-
+require_once 'NewJoyPla/core/HttpRequest.php';
+require_once 'NewJoyPla/core/HttpRequestParameter.php';
+require_once 'NewJoyPla/lib/Spiralv2LogginObject.php';
 
 function number_format_jp($num)
 {
-    if(empty($num)) { return 0; }
-    return preg_replace("/\.?0+$/","",number_format($num,2));
+    if (empty($num)) {
+        return 0;
+    }
+    return preg_replace('/\.?0+$/', '', number_format($num, 2));
 }
 
 /** 初期実行しなければならない処理 */
@@ -52,42 +53,56 @@ const JOYPLA_API_LOGGING_DB_TITLE = '73304'; // SPIRALv1v2オブジェクトで�
 const LOGGING_APP_TITLE = '24083'; // SPIRALv1v2オブジェクトで出力する場合に設定するAPPタイトル
 const SPIRALV2_API_KEY = 'dGFvQlZ9VUU4emE4TDMwbnp4T0hiUiRd'; // SPIRALv1v2オブジェクトで出力する場合に設定するAPPタイトル
 
-ApiSpiral::$logger = new Logger( new Spiralv2LogginObject( SPIRALV2_API_KEY , LOGGING_APP_TITLE ,SPIRAL_API_LOGGING_DB_TITLE  ) );
-ApiResponse::$logger = new Logger( new Spiralv2LogginObject( SPIRALV2_API_KEY , LOGGING_APP_TITLE ,JOYPLA_API_LOGGING_DB_TITLE  ) );
-
+ApiSpiral::$logger = new Logger(
+    new Spiralv2LogginObject(
+        SPIRALV2_API_KEY,
+        LOGGING_APP_TITLE,
+        SPIRAL_API_LOGGING_DB_TITLE
+    )
+);
+ApiResponse::$logger = new Logger(
+    new Spiralv2LogginObject(
+        SPIRALV2_API_KEY,
+        LOGGING_APP_TITLE,
+        JOYPLA_API_LOGGING_DB_TITLE
+    )
+);
 
 use App\Lib\UserInfo;
 
-class AutoLoad { 
-    
+class AutoLoad
+{
     public function __construct()
     {
         global $SPIRAL;
         $user_info = new UserInfo($SPIRAL);
-        if($user_info->getTermsAgreement() != '2' && $user_info->isDistributorUser())
-        {
-            require_once "NewJoyPla/autoload.php";
-            require_once "NewJoyPla/controller/Distributor/AgreePageController.php";
-            exit;
-        }
-        else if($user_info->getTermsAgreement() != '2' && $user_info->isHospitalUser())
-        {
-            require_once "NewJoyPla/autoload.php";
-            require_once "NewJoyPla/controller/AgreePageController.php";
-            exit;
-        }
-        else if($user_info->getAffiliationId() == "" && $user_info->isDistributorUser())
-        {
+        if (
+            $user_info->getTermsAgreement() != '2' &&
+            $user_info->isDistributorUser()
+        ) {
+            require_once 'NewJoyPla/autoload.php';
+            require_once 'NewJoyPla/controller/Distributor/AgreePageController.php';
+            exit();
+        } elseif (
+            $user_info->getTermsAgreement() != '2' &&
+            $user_info->isHospitalUser()
+        ) {
+            require_once 'NewJoyPla/autoload.php';
+            require_once 'NewJoyPla/controller/AgreePageController.php';
+            exit();
+        } elseif (
+            $user_info->getAffiliationId() == '' &&
+            $user_info->isDistributorUser()
+        ) {
             $autoload_action = 'accountSelect';
             require_once 'NewJoyPla/autoload.php';
             require_once 'NewJoyPla/controller/Distributor/TopPageController.php';
-            exit;
+            exit();
         }
     }
 }
 global $SPIRAL;
 $autoloadCheckSkip = $SPIRAL->getParam('autoloadCheckSkip');
-if($autoloadCheckSkip === null)
-{
+if ($autoloadCheckSkip === null) {
     $AutoLoad = new AutoLoad();
 }
