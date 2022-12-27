82061c1ae483a9685bf4949ee1b2953f3c42e46b
iida <iida.saaya@pi-pe.co.jp>
2022-12-27 17:32:42 +0900

    名称変更

--- src/NewJoyPla/controller/ItemRequestHistoryListController.php
+++ src/NewJoyPla/controller/ItemRequestHistoryListController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use View;
@@ -8,8 +9,6 @@ use ApiResponse;
 use Csrf;
 
 use App\Lib\UserInfo;
-use App\Model\Division;
-
 use ApiErrorCode\FactoryApiErrorCode;
 use stdClass;
 use Exception;
@@ -19,89 +18,28 @@ class ItemRequestHistoryListController extends Controller
     public function __construct()
     {
     }
-    
-    
+
+
     public function index(): View
     {
         global $SPIRAL;
         try {
 
             $user_info = new UserInfo($SPIRAL);
-            
-            if ($user_info->isDistributorUser())
-            {
-                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(),FactoryApiErrorCode::factory(404)->getCode());
+
+            if ($user_info->isDistributorUser()) {
+                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(), FactoryApiErrorCode::factory(404)->getCode());
             }
-            
-            if($user_info->isUser())
-            {
+
+            if ($user_info->isUser()) {
                 $content = $this->view('NewJoyPla/view/template/DivisionSelectList', [
                     'table' => '%sf:usr:search68:table%',
-                    'title' => '請求実績 - 部署選択',
+                    'title' => '請求履歴詳細一覧 - 部署選択',
                     'param' => 'ItemRequestHistoryListForDivision',
-                    ] , false);
-            }
-            else
-            {
+                ], false);
+            } else {
                 $content = $this->view('NewJoyPla/view/template/List', [
-                        'title' => '請求実績',
-                        'print' => false,
-                        'export' => false,
-                        'submenulink' => "%url/rel:mpg:top%&path=trackrecord",
-                        'submenu' => '実績メニュー',
-                        'table' => '%sf:usr:search67:mstfilter%',
-                        'csrf_token' => Csrf::generate(16),
-                        'script' => ''
-                        ] , false);
-            }
-        
-        } catch ( Exception $ex ) {
-            $content = $this->view('NewJoyPla/view/template/Error', [
-                'code' => $ex->getCode(),
-                'message'=> $ex->getMessage(),
-                ] , false);
-        } finally {
-            
-            $head = $this->view('NewJoyPla/view/template/parts/Head', [] , false);
-            $header = $this->view('NewJoyPla/src/HeaderForMypage', [
-                'SPIRAL' => $SPIRAL
-            ], false);
-            // テンプレートにパラメータを渡し、HTMLを生成し返却
-            return $this->view('NewJoyPla/view/template/Template', [
-                'title' => 'JoyPla 請求実績',
-                'script' => '',
-                'content' => $content->render(),
-                'head' => $head->render(),
-                'header' => $header->render(),
-                'baseUrl' => '',
-            ],false);
-        }
-    }
-    
-    public function ItemRequestHistoryListForDivision(): View
-    {
-        global $SPIRAL;
-        try {
-
-            $user_info = new UserInfo($SPIRAL);
-            
-            if ($user_info->isDistributorUser())
-            {
-                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(),FactoryApiErrorCode::factory(404)->getCode());
-            }
-            
-            if ($user_info->isHospitalUser() && ( $user_info->isApprover() || $user_info->isAdmin()) )
-            {
-                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(),FactoryApiErrorCode::factory(404)->getCode());
-            }
-            
-            if ( \App\lib\isMypage() )
-            {
-                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(),FactoryApiErrorCode::factory(404)->getCode());
-            }
-            
-            $content = $this->view('NewJoyPla/view/template/List', [
-                    'title' => '請求実績',
+                    'title' => '請求履歴詳細一覧',
                     'print' => false,
                     'export' => false,
                     'submenulink' => "%url/rel:mpg:top%&path=trackrecord",
@@ -109,31 +47,82 @@ class ItemRequestHistoryListController extends Controller
                     'table' => '%sf:usr:search67:mstfilter%',
                     'csrf_token' => Csrf::generate(16),
                     'script' => ''
-                    ] , false);
-    
-        } catch ( Exception $ex ) {
+                ], false);
+            }
+        } catch (Exception $ex) {
             $content = $this->view('NewJoyPla/view/template/Error', [
                 'code' => $ex->getCode(),
-                'message'=> $ex->getMessage(),
-                ] , false);
+                'message' => $ex->getMessage(),
+            ], false);
         } finally {
-            $head = $this->view('NewJoyPla/view/template/parts/Head', [] , false);
+
+            $head = $this->view('NewJoyPla/view/template/parts/Head', [], false);
             $header = $this->view('NewJoyPla/src/HeaderForMypage', [
                 'SPIRAL' => $SPIRAL
             ], false);
             // テンプレートにパラメータを渡し、HTMLを生成し返却
-            
             return $this->view('NewJoyPla/view/template/Template', [
-                'title' => 'JoyPla 請求実績',
+                'title' => 'JoyPla 請求履歴詳細一覧',
                 'script' => '',
                 'content' => $content->render(),
                 'head' => $head->render(),
                 'header' => $header->render(),
                 'baseUrl' => '',
-            ],false);
+            ], false);
         }
     }
 
+    public function ItemRequestHistoryListForDivision(): View
+    {
+        global $SPIRAL;
+        try {
+
+            $user_info = new UserInfo($SPIRAL);
+
+            if ($user_info->isDistributorUser()) {
+                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(), FactoryApiErrorCode::factory(404)->getCode());
+            }
+
+            if ($user_info->isHospitalUser() && ($user_info->isApprover() || $user_info->isAdmin())) {
+                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(), FactoryApiErrorCode::factory(404)->getCode());
+            }
+
+            if (\App\lib\isMypage()) {
+                throw new Exception(FactoryApiErrorCode::factory(404)->getMessage(), FactoryApiErrorCode::factory(404)->getCode());
+            }
+
+            $content = $this->view('NewJoyPla/view/template/List', [
+                'title' => '請求履歴詳細一覧',
+                'print' => false,
+                'export' => false,
+                'submenulink' => "%url/rel:mpg:top%&path=trackrecord",
+                'submenu' => '実績メニュー',
+                'table' => '%sf:usr:search67:mstfilter%',
+                'csrf_token' => Csrf::generate(16),
+                'script' => ''
+            ], false);
+        } catch (Exception $ex) {
+            $content = $this->view('NewJoyPla/view/template/Error', [
+                'code' => $ex->getCode(),
+                'message' => $ex->getMessage(),
+            ], false);
+        } finally {
+            $head = $this->view('NewJoyPla/view/template/parts/Head', [], false);
+            $header = $this->view('NewJoyPla/src/HeaderForMypage', [
+                'SPIRAL' => $SPIRAL
+            ], false);
+            // テンプレートにパラメータを渡し、HTMLを生成し返却
+
+            return $this->view('NewJoyPla/view/template/Template', [
+                'title' => 'JoyPla 請求履歴詳細一覧',
+                'script' => '',
+                'content' => $content->render(),
+                'head' => $head->render(),
+                'header' => $header->render(),
+                'baseUrl' => '',
+            ], false);
+        }
+    }
 }
 
 /***
@@ -141,15 +130,10 @@ class ItemRequestHistoryListController extends Controller
  */
 $ItemRequestHistoryListController = new ItemRequestHistoryListController();
 
-$action = $SPIRAL->getParam('Action');
-
-{
-    if($action === 'ItemRequestHistoryListForDivision')
-    {
+$action = $SPIRAL->getParam('Action'); {
+    if ($action === 'ItemRequestHistoryListForDivision') {
         echo $ItemRequestHistoryListController->ItemRequestHistoryListForDivision()->render();
-    }
-    else
-    {
+    } else {
         echo $ItemRequestHistoryListController->index()->render();
     }
 }
