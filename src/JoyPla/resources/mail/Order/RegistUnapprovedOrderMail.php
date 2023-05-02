725cdd9dbd8fc6bb45c607edceef4309139448f7
APKAVIFL7EWRG4EKB4U4 <ito.shun@pi-pe.co.jp>
2023-05-02 16:00:45 +0900

    save

--- src/JoyPla/resources/mail/Order/RegistUnapprovedOrderMail.php
+++ src/JoyPla/resources/mail/Order/RegistUnapprovedOrderMail.php
@@ -5,6 +5,14 @@ JoyPla からお知らせです。
 未発注書が作成されましたので、下記の通りお知らせします。
 
 <?php
+$order_methods = [
+    '1' => 'JoyPla',
+    '2' => 'メール',
+    '3' => 'FAX',
+    '4' => '電話',
+    '5' => '業者システム',
+];
+
 echo '施設名: ' . $hospitalName . " \n";
 echo '担当者: ' . $ordererUserName . " \n";
 echo " \n";
@@ -13,29 +21,13 @@ foreach ($history as $h) {
     echo '発注番号: ' . $h['orderNumber'] . " \n";
     echo '部署名: ' . $h['divisionName'] . " \n";
     echo '卸業者名: ' . $h['distributorName'] . " \n";
-    echo '発注方法: ' . orderMethod($h['orderMethod']) . " \n";
+    echo '発注方法: ' .
+        (isset($order_methods[$h['orderMethod']])
+            ? $order_methods[$h['orderMethod']]
+            : 'その他') .
+        " \n";
     echo '金額: ￥' . $h['totalAmount'] . " \n";
     echo " \n";
-}
-
-function orderMethod($index)
-{
-    if ($index == '1') {
-        return 'JoyPla';
-    }
-    if ($index == '2') {
-        return 'メール';
-    }
-    if ($index == '3') {
-        return 'FAX';
-    }
-    if ($index == '4') {
-        return '電話';
-    }
-    if ($index == '5') {
-        return '業者システム';
-    }
-    return 'その他';
 }
 ?>
 
