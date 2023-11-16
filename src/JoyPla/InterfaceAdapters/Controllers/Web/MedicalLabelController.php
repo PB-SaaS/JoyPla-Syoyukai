d7b4222fc2d8a74c3b8cb7e387f50e61f413dc69
George Uda <georgeudandy@gmail.com>
2023-11-16 18:04:45 +0900

    merge

--- src/JoyPla/InterfaceAdapters/Controllers/Web/MedicalLabelController.php
+++ src/JoyPla/InterfaceAdapters/Controllers/Web/MedicalLabelController.php
@@ -1,18 +1,149 @@
 <?php
 
-namespace JoyPla\InterfaceAdapters\Controllers\Web ;
+namespace JoyPla\InterfaceAdapters\Controllers\Web;
  
 use Exception;
 use framework\Http\Request;
 use framework\Http\Controller;
 use framework\Http\View;
- 
+ use JoyPla\Enterprise\Models\HospitalId;
+use JoyPla\Enterprise\Models\DivisionId;
+use JoyPla\Enterprise\Models\InHospitalItemId;
+use JoyPla\InterfaceAdapters\GateWays\ModelRepository;
+use JoyPla\Service\Repository\RepositoryProvider;
+
 class MedicalLabelController extends Controller
 {
     public function __construct(Request $request)
     {
         parent::__construct($request);
     }
+
+    public function MedicalOrderLabelPrint(array $vars)
+    {
+        $orderId = $vars['orderId'];
+
+        $request = $this->request->get('request' , []);
+
+        $order = ModelRepository::getOrderInstance()
+        ->where('hospitalId', $this->request->user()->hospitalId)
+        ->where('orderNumber', $orderId)
+        ->get()
+        ->first();
+
+        $inHospitalItemIds = [];
+
+        $orderItems = ModelRepository::getOrderItemInstance()
+        ->where('hospitalId', $this->request->user()->hospitalId)
+        ->where('orderNumber', $orderId)
+        ->get();
+
+        $orderItems = $orderItems->toArray();
+
+        foreach ($orderItems as $orderItem) {
+            $inHospitalItemIds[] = $orderItem['inHospitalItemId'];
+        }
+
+
+        foreach( $orderItems as $orderItem )
+        {
+            $requestItem = array_find($request, function($req) use ($orderItem){
+                return $orderItem['orderCNumber'] === $req['orderCNumber'];
+            });
+
+            $print = [];
+            if(empty($requestItem['print']))
+            {
+                $print[] = [
+                    'count' => $orderItem['quantity'], //数量欄
+                    'print' => $orderItem['orderQuantity'], //多分枚数欄：発注数ベース
+                ];
+            } else {
+                $print = $requestItem['print'];
+            }
+
+            $requests[] = [
+                'orderCNumber'     => $orderItem['orderCNumber'],
+                'inHospitalItemId' => $orderItem['inHospitalItemId'],
+                'orderQuantity'    => $orderItem['orderQuantity'],
+                'print'            => $print,
+            ];
+        }
+
+        $requestPrint = $requests;
+
+        $repository = new RepositoryProvider();
+        $hospital = $repository->getHospitalRepository()->findRow(new HospitalId($this->request->user()->hospitalId));
+        $divisionId = new DivisionId($order->divisionId);
+        $division = $repository->getDivisionRepository()->find(new HospitalId($order->hospitalId), new DivisionId($order->divisionId));
+
+        $inHospitalItems = $repository->getInHospitalItemRepository()->getInHospitalItemViewByInHospitalItemIds(
+            new HospitalId($this->request->user()->hospitalId),
+            array_map(function($item){
+                return new InHospitalItemId($item['inHospitalItemId']);
+            },$requestPrint)
+        );
+
+
+
+        // ob_start();
+        // print_r($request);
+        // $result = ob_get_clean();
+        // $htmlList = "<pre>" . $result . "</pre>";
+
+        // $listItems = array_map(function($itemId) {
+        //     return "<li>{$itemId}</li>";
+        // }, $inHospitalItemIds);
+        
+        // // リストの開始と終了タグを追加
+        // $htmlList = "<ul>" . implode('', $listItems) . "</ul>";
+
+
+
+        foreach($inHospitalItems as $key => $item)
+        {
+            foreach($requestPrint as $rKey => $printItem)
+            {
+                if($item->inHospitalItemId === $printItem['inHospitalItemId'])
+                {
+                    if(!$inHospitalItems[$key]->order){
+                        $inHospitalItems[$key]->order = [];
+                    }
+                    $inHospitalItems[$key]->order[] = $printItem;
+                }
+                $inHospitalItems[$key]->quantity = $printItem['orderQuantity'];
+            }
+            $inHospitalItems[$key]->divisionName = $division->getDivisionName()->value();
+        }
+
+        $inHospitalItems = array_map(function($inHospitalItem){
+            return $inHospitalItem->toArray();
+        },$inHospitalItems);
+
+        $labeldesign = $hospital->labelDesign2 !== '' ?  $hospital->labelDesign2 : $this->defaultDesign2();
+
+        $words = $this->convertInputDateForOrder($inHospitalItems);
+        $body = View::forge('html/Label/Payout', [
+            'orderId' => $orderId,
+            'inHospitalItems' => $inHospitalItems,
+            'totalPrintCount' => count($words),
+            'labelHtml' => $this->convertKeyword($labeldesign , $words),
+            // 'inHospitalItems' => array(),
+            // 'totalPrintCount' => 3,
+            // 'labelHtml' => <<<EOM
+            // <div class='printarea uk-margin-remove'>
+            //     <div>
+            //         <b class='font-size-16'>aaaaaaaaaa</b>
+            //         $htmlList
+            //     </div>
+            // </div>
+            // EOM,
+        ], false)->render();
+
+        echo view('html/Common/Template', compact('body'), false)->render();
+
+    }
+    public function MedicalReceivedLabelPrint(array $vars)
  
     public function index(array $vars)
     {
@@ -81,194 +212,10 @@ class MedicalLabelController extends Controller
 
     private function defaultDesign()
     {
-        return <<<EOM
-    <div class="printarea uk-margin-remove">
-        <div class="uk-child-width-1-2 uk-grid" uk-grid="">
-            <div class="uk-first-column">
-                <b class="font-size-16">償還</b><br>
-                <span>%itemName%</span><br>
-                <span>%makerName%</span><br>
-            </div>
-            <div class="uk-text-right uk-padding-remove">
-                <span>%printDate%</span><br>
-                <b>入数単位:1%quantityUnit%</b><br>
-                <b>償還価格:%officialprice%円</b><br>
-            </div>
-        </div>
-        <span>%catalogNo%</span><br>
-        <span>%itemStandard%</span><br>
-        <span>%medicineCategory%</span><br>
-        <br>
-        <span>%distributorName%</span><br>
 
-	</div>
-EOM;
     }
-
-    
-    private function mockHospitalItem()
+    public function MedicalPayoutLabelPrint(array $vars)
     {
-        return [
-            [
-                "id" => "1",
-                "registrationTime" => "2023年10月6日 09時10分53秒",
-                "updateTime" => "2023年10月18日 13時30分30秒",
-                "itemId" => "item_0000000002",
-                "priceId" => "price_0000000001",
-                "inHospitalItemId" => "00000001",
-                "authKey" => "h4a23cg4ynf35326",
-                "hospitalId" => "hospital_0000000001",
-                "medicineCategory" => "アルコール毛細管体温計\n単回使用気管切開チューブ",
-                "homeCategory" => "",
-                "notUsedFlag" => "0",
-                "notice" => "",
-                "HPstock" => "3073",
-                "unitPrice" => "1000",
-                "measuringInst" => "",
-                "makerName" => "エンブレム株式会社",
-                "itemName" => "消毒用アルコール綿",
-                "itemCode" => "",
-                "itemStandard" => "M",
-                "itemJANCode" => "4987603115095",
-                "officialFlag" => "1",
-                "officialpriceOld" => "",
-                "officialprice" => "2000",
-                "catalogNo" => "01231809",
-                "serialNo" => "",
-                "lotManagement" => "",
-                "category" => "1",
-                "distributorId" => "dis_1014796949",
-                "distributorName" => "Cell医療薬品株式会社",
-                "price" => "1000",
-                "quantity" => "100",
-                "itemUnit" => "枚",
-                "quantityUnit" => "箱",
-                "distributorMCode" => "",
-                "inItemImage" => "",
-                "labelId" => "00000001",
-                "payout" => [
-                    [
-                        "payoutItemId" => "payout_0000000001",
-                        "inHospitalItemId" => "00000001",
-                        "lotNumber" => "rotto012",
-                        "lotDate" => "2099年10月01日",
-                        "print" => [
-                            [
-                                "count" => "1",
-                                "print" => "1"
-                            ]
-                        ]
-                    ]
-                ],
-                "target" => [
-                    "divisionId" => "div_0833326238",
-                    "divisionName" => "リハビリテーション科",
-                    "inHospitalItemId" => "00000001",
-                    "rackName" => "",
-                    "constantByDiv" => "10"
-                ],
-                "source" => [
-                    "divisionId" => "div_0053498963",
-                    "divisionName" => "内科",
-                    "inHospitalItemId" => "00000001",
-                    "rackName" => "",
-                    "constantByDiv" => "10"
-                ]
-                ],
-                [
-                    "id" => "1",
-                    "registrationTime" => "2023年10月6日 09時10分53秒",
-                    "updateTime" => "2023年10月18日 13時30分30秒",
-                    "itemId" => "item_0000000002",
-                    "priceId" => "price_0000000001",
-                    "inHospitalItemId" => "00000001",
-                    "authKey" => "h4a23cg4ynf35326",
-                    "hospitalId" => "hospital_0000000001",
-                    "medicineCategory" => "アルコール毛細管体温計\n単回使用気管切開チューブ",
-                    "homeCategory" => "",
-                    "notUsedFlag" => "0",
-                    "notice" => "",
-                    "HPstock" => "3073",
-                    "unitPrice" => "1000",
-                    "measuringInst" => "",
-                    "makerName" => "グリコ株式会社",
-                    "itemName" => "ポッキー",
-                    "itemCode" => "",
-                    "itemStandard" => "M",
-                    "itemJANCode" => "4987603115095",
-                    "officialFlag" => "1",
-                    "officialpriceOld" => "",
-                    "officialprice" => "2000",
-                    "catalogNo" => "01231809",
-                    "serialNo" => "",
-                    "lotManagement" => "",
-                    "category" => "1",
-                    "distributorId" => "dis_1014796949",
-                    "distributorName" => "Cell医療薬品株式会社",
-                    "price" => "1000",
-                    "quantity" => "100",
-                    "itemUnit" => "枚",
-                    "quantityUnit" => "箱",
-                    "distributorMCode" => "",
-                    "inItemImage" => "",
-                    "labelId" => "00000001",
-                    "payout" => [
-                        [
-                            "payoutItemId" => "payout_0000000002",
-                            "inHospitalItemId" => "00000001",
-                            "lotNumber" => "rotto012",
-                            "lotDate" => "2099年10月01日",
-                            "print" => [
-                                [
-                                    "count" => "1",
-                                    "print" => "2"
-                                ]
-                            ]
-                        ]
-                    ],
-                    "target" => [
-                        "divisionId" => "div_0833326238",
-                        "divisionName" => "リハビリテーション科",
-                        "inHospitalItemId" => "00000001",
-                        "rackName" => "",
-                        "constantByDiv" => "10"
-                    ],
-                    "source" => [
-                        "divisionId" => "div_0053498963",
-                        "divisionName" => "内科",
-                        "inHospitalItemId" => "00000001",
-                        "rackName" => "",
-                        "constantByDiv" => "10"
-                    ]
-                    ],
-                
-        ];
-    }
-    
-    private function convertKeyword(string $template, array $inputData){
-        $html = '';
-        foreach($inputData as $key => $input)
-        {
-            $design = $template;
-            $design = str_replace('%itemName%',			$input['itemName'],                 $design);//商品名
-            $design = str_replace('%makerName%',		$input['makerName'], 		        $design);//メーカー名
-            $design = str_replace('%printDate%',		$this->getToday(), 					$design);//印刷日
-            $design = str_replace('%quantityUnit%',		$input['quantityUnit'],	            $design);//入数単位
-            $design = str_replace('%officialprice%',	number_format_jp((float)$input['officialprice']),  $design);//償還価格
-            $design = str_replace('%catalogNo%',		$input['catalogNo'], 		        $design);//カタログNo
-            $design = str_replace('%itemStandard%',		$input['itemStandard'], 		    $design);//規格
-            $replacedMedicineCategory = str_replace("\n", '<br>', $input['medicineCategory']);
-            $design = str_replace('%medicineCategory%',	$replacedMedicineCategory, 		$design);//(特定保険材料名称「保険請求分類(医科)」)
-            $design = str_replace('%distributorName%',		$input['distributorName'], 		        $design);//卸業者            
-            $html .= $design;
-        }   
-        return $html;
-    }
 
-    private function getToday($timezone = 'Asia/Tokyo') {
-        date_default_timezone_set($timezone);
-        return date('y/m/d');
     }
-
-    
 }
