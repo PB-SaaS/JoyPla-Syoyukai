91e7e6b040c50ec0e771f33bb5266652b9c7eb9c
kakiuchi-takahiro <103478999+kakiuchi-takahiro@users.noreply.github.com>
2023-11-16 11:33:56 +0900

    発注商品ラベルのバーコード読み込み時の出力・挙動追加

--- src/JoyPla/resources/html/Consumption/ConsumptionRegister.php
+++ src/JoyPla/resources/html/Consumption/ConsumptionRegister.php
@@ -556,6 +556,12 @@ var JoyPlaApp = Vue.createApp({
             items.item[id].consumeQuantity = parseInt(items.item[id].customQuantity);
           });
         }
+        if(items.type == "order")
+        {
+          items.item.forEach((x , id)=>{
+            items.item[id].consumeUnitQuantity = 1; //parseInt(items.item[id].orderQuantity);
+          });
+        }
 
         if (items.item.length === 1) {
           if(items.item[0].divisionId)
